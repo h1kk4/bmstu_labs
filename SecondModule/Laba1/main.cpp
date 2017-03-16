97a1e8f17df940a71c0a381631240f4639546d4b
h1kk4 <alex@MacBook-Air-Aleksej-2.local>
2017-03-16 22:25:45 +0300

    it's works!

--- SecondModule/Laba1/main.cpp
+++ SecondModule/Laba1/main.cpp
@@ -11,9 +11,10 @@ int intFromString(const char *data) {
         if (isalpha(data[i])) {
             throw WrongChar();
         }
-        if(data[i]=='.'||data[i]==',') break;
+        if (data[i] == '.' || data[i] == ',') break;
         k++;
-        if (data[0]=='-') continue;
+
+        if (data[0] == '-') continue;
         if (flag && (data[i] == '0')) {
 
         } else {
@@ -26,7 +27,7 @@ int intFromString(const char *data) {
 
     }
 
-    if (size >11 ) {
+    if (size > 11) {
         throw OverFlow();
     } else {
         if (data[0] != '-') {
@@ -38,9 +39,9 @@ int intFromString(const char *data) {
                 x = -((int) ((int(data[k - i - 1]) - 48) * pow(10, i))) + x;
             }
         }
-        if((x>0&&data[0]!='-')||(x<0&&data[0]=='-'))
+        if ((x >= 0 && data[0] != '-') || (x <= 0 && data[0] == '-'))
             return x;
-        else{
+        else {
             throw OverFlow();
         }
 
@@ -53,8 +54,8 @@ bool boolFromString(const char *data) {
     std::string str(data);
     int i(0);
 
-    while (i!=str.size()) {
-        str[i]=char(tolower(data[i]));
+    while (i != str.size()) {
+        str[i] = char(tolower(data[i]));
         i++;
 
     }
@@ -71,6 +72,7 @@ float floatFromString(const char *data) {
     float x(0);
     for (int i = 0; data[i] != '\0'; i++) {
         k++;
+        //std::cout<<"k-"<<'('<<k<<')'<<'\n';
         if (isalpha(data[i])) {
             throw WrongChar();
         }
@@ -80,12 +82,8 @@ float floatFromString(const char *data) {
         }
 
     }
-    if(count>1) throw WrongChar();
-    if (dot==0){
-        for (int i = 0; i < k; i++) {
-            x = (int) ((int(data[k - i - 1]) - 48) * pow(10, i)) + x;
-        }
-    }
+    if (count > 1) throw WrongChar();
+
     if (data[0] != '-') {
         for (int i = 0; i < dot; i++) {
             x = (float) ((int(data[dot - i - 1]) - 48) * pow(10, i)) + x;
@@ -98,11 +96,11 @@ float floatFromString(const char *data) {
     } else {
         for (int i = 0; i < dot - 1; i++) {
             x = -((float) ((int(data[dot - i - 1]) - 48) * pow(10, i))) + x;
-            //std::cout<<"Before"<<i<<" "<<x<<'\n';
+            //std::cout << "Before" << i << " " << x << '\n';
         }
         for (int i = 1; i < k - dot; i++) {
             x = -((float) ((int(data[dot + i]) - 48) * pow(10, -i))) + x;
-            //std::cout<<i<<" "<<x<<'\n';
+            //std::cout << i << " " << x << '\n';
         }
     }
 
@@ -111,10 +109,11 @@ float floatFromString(const char *data) {
 
 int main() {
     //std::cout << std::numeric_limits<int>::min() <<'\n'<< std::numeric_limits<int>::max()<<'\n';
-    std::cout<<1<<'='<<intFromString("-00214748364")<<'\n';
-    std::cout<<2<<'='<<intFromString("-002147483648")<<'\n';
-    std::cout<<3<<'='<<intFromString("002147483647")<<'\n';
-    std::cout<<4<<'='<<intFromString("0021474.83647")<<'\n'; // Должно получится 21474
+    std::cout << 1 << '=' << intFromString("-00214748364") << '\n';
+    std::cout << 2 << '=' << intFromString("-002147483648") << '\n';
+    std::cout << 3 << '=' << intFromString("002147483647") << '\n';
+    std::cout << 4 << '=' << intFromString("0021474.83647") << '\n'; // Должно получится 21474
+    std::cout << 5 << '=' << floatFromString("0") << '\n';
     try {
         intFromString("-002147483649");
     }
@@ -128,37 +127,34 @@ int main() {
         exc.what();
     }
 
-    std::cout<<floatFromString("00003333")<<'\n';
-    std::cout<<floatFromString("000001.2343")<<'\n';
+    std::cout << floatFromString("00003333") << '\n';
+    std::cout << floatFromString("000001.2343") << '\n';
     try {
-        std::cout<<"--->"<<floatFromString("000001.2343.4")<<'\n';
+        std::cout << "--->" << floatFromString("000001.2343.4") << '\n';
     }
     catch (WrongChar &exc) {
         exc.what();
     }
     try {
-        std::cout<<floatFromString("000001.2343d4")<<'\n';
+        std::cout << floatFromString("000001.2343d4") << '\n';
     }
     catch (WrongChar &exc) {
         exc.what();
     }
     try {
-        std::cout<<floatFromString("00d0001.23434")<<'\n';
+        std::cout << floatFromString("00d0001.23434") << '\n';
     }
     catch (WrongChar &exc) {
         exc.what();
     }
 
     boolFromString("FalSe");
-    try
-    {
+    try {
         boolFromString("FalSse");
     }
-    catch (WrongChar &exc)
-    {
+    catch (WrongChar &exc) {
         exc.what();
     }
-
 
 
     return 0;
