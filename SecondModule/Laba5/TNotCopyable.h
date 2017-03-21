4e358cac0ef861b74f55916487fd32379367c1dc
h1kk4 <alex@MacBook-Air-Aleksej-2.local>
2017-03-21 20:05:12 +0300

    Fix

--- SecondModule/Laba5/TNotCopyable.h
+++ SecondModule/Laba5/TNotCopyable.h
@@ -5,77 +5,71 @@
 #ifndef MODULE2LABA5_TNOTCOPYABLE_H
 #define MODULE2LABA5_TNOTCOPYABLE_H
 #pragma warning(disable:4996)
+
 #include <exception>
 #include <cstdio>
 #include <string>
 #include <iostream>
+
 struct FileAlreadyOpened
-        : public std::exception
-{
+        : public std::exception {
     FileAlreadyOpened() = default;
-    FileAlreadyOpened(const char* const message)
-            : std::exception()
-    { }
+
+    FileAlreadyOpened(const char *const message)
+            : std::exception() {}
 };
 
-class TNotCopyable
-{
-    FILE * Descriptor;
+class TNotCopyable {
+    FILE *Descriptor;
 
 public:
     TNotCopyable()
-            : Descriptor(nullptr)
-    { }
+            : Descriptor(nullptr) {}
 
-    TNotCopyable(const TNotCopyable&) = delete;
-    TNotCopyable& operator = (const TNotCopyable&) = delete;
+    TNotCopyable(const TNotCopyable &) = delete;
 
-    void OpenToWrite(std::string const & fileName)
-    {
+    TNotCopyable &operator=(const TNotCopyable &) = delete;
+
+    void OpenToWrite(std::string const &fileName) {
         if (Descriptor != nullptr)
             throw FileAlreadyOpened();
         Descriptor = fopen(fileName.c_str(), "w");
     }
 
-    void OpenToRead(std::string const & fileName)
-    {
+    void OpenToRead(std::string const &fileName) {
         if (Descriptor != nullptr)
             throw FileAlreadyOpened();
         Descriptor = fopen(fileName.c_str(), "r");
     }
 
-    void Close() throw()
-    {
-        if (Descriptor != nullptr)
-        {
+    void Close() throw() {
+        if (Descriptor != nullptr) {
             fclose(Descriptor);
             Descriptor = nullptr;
         }
     }
 
-    FILE * Get()
-    {
+    FILE *Get() {
         return Descriptor;
     }
 
 
-
 };
 
-class RAII{
+class RAII {
 private:
     TNotCopyable *Ptr;
 public:
-    RAII(){}
-    RAII(TNotCopyable *ptr):Ptr(ptr)
-    {
-       std::cout<<"ctor"<<'\n';
+    RAII() {}
+
+    RAII(TNotCopyable *ptr) : Ptr(ptr) {
+
     }
 
-    ~RAII()
-    {
-        std::cout << "dctor"<<'\n';
-        Ptr->Close();
+    ~RAII() {
+        if (Ptr)
+            Ptr->Close();
     }
 };
+
 #endif //MODULE2LABA5_TNOTCOPYABLE_H
